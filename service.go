bfa82cd37f76f05e5ef5786451521402f4fdcc72
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2022-07-10 11:09:52 +0700

    refactor source code

--- service.go
+++ service.go
@@ -1,1 +1,22 @@
 package main
+
+import "strings"
+
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
